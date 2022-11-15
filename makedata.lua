4469fb3e649ffa9523e05e574698149114b08956
PhytoEpidemic <64293310+PhytoEpidemic@users.noreply.github.com>
2022-11-14 19:44:30 -0600

    Many more functions
    You can now choose to remove blurry frames and/or only keep key frames

--- makedata.lua
+++ makedata.lua
@@ -1,4 +1,198 @@
-print("video file")
-local vfile = '"'..(io.read():gsub('"',""))..'"'
+lfs = require("lfs")
+imagedim = require("imagedim")
+function exe(str)
+	os.execute(str)
+end
+function stitle(s)
+	exe([[title vid2data.exe ]]..s)
+end
+function rtitle()
+	stitle("")
+end
+function cls()
+	os.execute("cls")
+end
+function pause()
+	os.execute("pause")
+end
+local unallowedNames = {
+["."] = true,
+[".."] = true,
+["desktop.ini"] = true,
+}
+local function isAllowed(n)
+	return not (unallowedNames[n] or false)
+end
+function string_findlast(str,pat)
+	local sspot,lspot = str:find(pat)
+	local lastsspot, lastlspot = sspot, lspot
+	while sspot do
+		lastsspot, lastlspot = sspot, lspot
+		sspot, lspot = str:find(pat,lastlspot+1)
+	end
+	return lastsspot, lastlspot
+end
+function folderUP(path,num)	
+	num = num or 1
+	local look = string_findlast(path,[[\]])
+	if look then 
+		local upafolder = path:sub(1,look-1)
+		if num > 1 then
+			return folderUP(upafolder,num-1)
+		else
+			return upafolder
+		end
+		
+	else
+		return ""
+	end
+end
+
+function endOfPath(f)
+	local prevPath = folderUP(f)
+	local cutspot = #prevPath
+	if cutspot == 0 then
+		cutspot = -1
+	end
+	return f:sub(cutspot+2,#f)
+end
+local function makeDir(path)
+	local snip = folderUP(path)
+	local attr = getAttributes(snip)
+	if attr and attr.mode == "directory" then
+	else makeDir(snip)
+	end
+	return lfs.mkdir(path)
+end
+function getEXT(file)
+	local extstart = string_findlast(file,"%.")
+	if extstart then--and extstart > ((file:find("\\")) or 0) then
+		return (file:sub(extstart+1,#file))
+	end
+	return ""
+end
+function padNum(str)
+	local fulldigits = [[00000000]]
+	str = str:gsub(" ","")
+	return (fulldigits:sub(#str+1,#fulldigits))..str
+end
+
+function splitImage(tw,th,ow,oh)
+	local cells = {}
+	local wsplits = ow/tw
+	local wfull = math.floor(wsplits)
+	local wextra = 1-(wsplits-wfull)
+	local wpadding = (tw*wextra)/wfull
+	local hsplits = oh/th
+	local hfull = math.floor(hsplits)
+	local hextra = 1-(hsplits-hfull)
+	local hpadding = (th*hextra)/hfull
+	
+	
+	for i=0,wfull do
+		for j=0,hfull do
+			local cell = {}
+			cell.w = tw
+			cell.h = th
+			cell.x = i*tw
+			if i>0 then
+				cell.x = cell.x-wpadding
+			end
+			
+			cell.y = j*th
+			if j>0 then
+				cell.y = cell.y-hpadding
+			end
+				
+			
+			
+			table.insert(cells,cell)
+		end
+	end
+	
+	
+	return cells
+end
+local config = {}
+function printset()
+	print("Selected Video: "..(config.vfile or "none"))
+	print("Key frames only: "..(config.keyframesonly or "n"))
+	print("Remove blurred frames: "..(config.removeblur or "n"))
+	
+end
+
+
+rtitle()
+print("Drag and drop your video file or type in the exact location.")
+config.vfile = (io.read():gsub('"',""))
+cls()
+printset()
+print("Key frames only?")
+config.keyframesonly = (io.read():gsub('"',""))
+cls()
+printset()
+print("Remove blurred frames?")
+config.removeblur = (io.read():gsub('"',""))
+local framesFolder = config.vfile..[[_frames]]
 print("")
-os.execute("fixframes "..vfile)
+exe([[mkdir "]]..framesFolder..[["]])
+--exe([[mkdir "]]..framesFolder..[[mp"]])
+if config.keyframesonly == "y" then
+	config.keyframesonly = [[ -skip_frame nokey]]
+else
+	config.keyframesonly = ""
+end
+exe([[ffmpeg.exe]]..config.keyframesonly..[[ -i "]]..config.vfile..[[" -vf mpdecimate,setpts=N/FRAME_RATE/TB,blurdetect=block_width=128:block_height=128:block_pct=95,metadata=print:file=log.txt "]]..framesFolder..[[/%08d.png"]])
+--exe([[ffmpeg.exe -i "]]..framesFolder..[[" -vf mpdecimate=hi=200:lo=200:frac=1:max=0,setpts=N/FRAME_RATE/TB "]]..framesFolder..[[mp/%08d.png"]])
+
+if config.removeblur == "y" then
+	local blurlogf = io.open("log.txt","r")
+	local framec = "0"
+	local blur = "0"
+	for line in blurlogf:lines() do
+		if line:find("frame") then
+			local ptss = line:find("pts")
+			framec = (line:sub(7,ptss-2)):gsub(" ","")
+			blur = "0"
+		end
+		
+		if line:find("lavfi.blur=") then
+			local _,skipstr = line:find("lavfi.blur=")
+			blur = (line:sub(skipstr+1,#line))
+		end
+		if framec ~= "0" and blur ~= "0" then
+			local filename = padNum(framec)..".png"
+			local blurnum = tonumber(blur)
+			if blurnum > 9 then
+				
+				os.remove(framesFolder.."\\"..filename)
+				print(filename)
+				print(blur)
+			end
+			
+		end
+	end
+end
+
+
+
+
+for file in lfs.dir(framesFolder) do
+	if isAllowed(file) then
+		local filepath = framesFolder.."\\"..file
+		if lfs.attributes(filepath).mode == "file" then
+			local width, height = imagedim.GetImageWidthHeight(filepath)
+			if width ~= 512 or height ~= 512 then
+				print(width,height)
+				local cells = splitImage(512,512,width,height)
+				for i,cell in ipairs(cells) do
+					os.execute([[ffmpeg -i "]]..filepath..[[" -vf "crop=]]..tostring(cell.w)..[[:]]..tostring(cell.h)..[[:]]..tostring(cell.x)..[[:]]..tostring(cell.y)..[[" "]]..filepath..[[_crop]]..tostring(i)..[[.png"]])
+				end
+				os.remove(filepath)
+			end
+		end
+	end
+end
+cls()
+
+pause()
